6f06aaf38fcf2d1ecd76bf7ee1e81f46901265da
Harish Thangaraj <harish.thangaraj2021@vitstudent.ac.in>
2025-02-23 21:25:40 +0530

    remoe publication header

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -74,13 +74,6 @@ const AppHeader = () => {
             className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
             aria-label="Contact"
           >
-            Publications
-          </Link>
-          <Link
-            to="/contact"
-            className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
-            aria-label="Contact"
-          >
             Contact
           </Link>
         </div>
