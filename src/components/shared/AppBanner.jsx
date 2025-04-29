1c7ae868de0cc106b5a3e05c2aac916ebc08cf93
Harish Thangaraj <108048779+rulezcasa@users.noreply.github.com>
2025-04-29 13:04:10 +0530

    Update AppBanner.jsx

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -47,7 +47,7 @@ const AppBanner = () => {
           className="flex justify-center"
         >
           <a
-            download="Harish-resume.pdf"
+            download="Harish-CV.pdf"
             href="/files/CV_Harish.pdf"
             className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500 text-center"
             aria-label="Download Resume"
